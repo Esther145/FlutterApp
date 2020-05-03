2c7f6155137c791a1c7b3635116ed277d9fb2ab7
Esther145 <esther.thomson98@gmail.com>
2020-05-04 01:56:00 +0400

    Login Page

--- main.dart
+++ main.dart
@@ -1,18 +1,49 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main()
-{
-  runApp(Hello());
+void main() {
+  runApp(MyApp());
 }
 
-class Hello extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.lime,
         appBar: AppBar(
-          title: Text("MBCET TVM"),
+          leading: Icon(
+            Icons.add_shopping_cart,
+            size: 50.0,
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.green,
+          title: Text("MBCET TVM",
+              style: TextStyle(fontSize: 25.0, letterSpacing: 2.0)),
+        ),
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              //Text("Username"),
+              TextField(
+                decoration: InputDecoration(
+                  hintText:"Enter Username "
+                ),
+              ),
+              //Text("Password"),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Enter Password"
+                ),
+              ),
+              RaisedButton(
+                color: Colors.green,
+                child: Text("Login",style: TextStyle(color: Colors.white),),
+                onPressed: () {},
+              )
+            ],
+          ),
         ),
       ),
     );
